01dd709ddb2325c20065399589e8cad2c061d9c8
Chetan Pareek <pareekchetan7891@gmail.com>
2021-12-04 15:35:47 +0530

    add

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const request_service = require("../controllers/request_service");
 const app = express();
 const register = require("./../controllers/register");
 
 app.route("/register_user").post(register.register_user);
 app.route("/register_mechanic").post(register.register_mechanic);
-
+app.route("/request_service").patch(request_service.request_service);
 app.route("/getMechanicDataById/:id").get(register.getMechanicData);
 
 module.exports = app;
